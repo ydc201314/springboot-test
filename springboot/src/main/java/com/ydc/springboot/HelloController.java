4e49c831052744630cdd3d1d278b8546dba8a0f2
taotao <ydc201314@163.com>
2018-09-26 08:53:07 +0800

    en

---
+++ springboot/src/main/java/com/ydc/springboot/HelloController.java
@@ -0,0 +1,13 @@
+package com.ydc.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String helloTest(){
+        return "hello world!";
+    }
+}
